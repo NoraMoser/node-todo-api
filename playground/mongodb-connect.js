86a4081f5de4a3f971b86e8b96ac344f4ceee719
Moser <noralmoser@gmail.com>
2018-04-13 13:12:36 -0500

    destructuring objects and mongoDB

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,10 +1,42 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// var user = {name: 'nora', age: 32};
+// var {name} = user;
+// console.log(name);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
         return console.log('Unable to connect to MongoDB server');
     }
     console.log('Connected to MongoDB server')
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    // db.collection('Nora').insertOne({
+    //     name: 'Nora',
+    //     age: 32,
+    //     location: 'here'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('so sorry');
+    //     }
+
+    //     // console.log(JSON.stringify(result.ops, undefined, 2));
+    //     console.log(result.ops[0]._id.getTimestamp())
+    // });
 
     db.close();
 });
