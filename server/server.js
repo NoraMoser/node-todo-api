f7d12fb3a37d9763a37e68d37fc6a19fc6f8fffb
Moser <noralmoser@gmail.com>
2018-04-20 15:41:03 -0500

    using postman to make a post in mongodb database

--- server/server.js
+++ server/server.js
@@ -1,24 +1,29 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp')
+var {mongoose} = require('./db/mongoose');
+var { Todo } = require('./models/todos')
+var { User } = require('./models/users')
 
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+}); 
+
+app.listen(3000, () => {
+    console.log('Started on port 3000')
 });
 
 // var newTodo = new Todo({
@@ -55,28 +60,13 @@ var Todo = mongoose.model('Todo', {
 //     console.log('will not work')
 // });
 
-var User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    }
-});
+// var createUser = new User({
+//     name: "Nora",
+//     email: "whatever@whatever.com"
+// })
 
-var createUser = new User({
-    name: "Nora",
-    email: "whatever@whatever.com"
-})
-
-createUser.save().then((doc) => {
-    console.log(doc);
-}, (e) => {
-    console.log('will not work')
-});
+// createUser.save().then((doc) => {
+//     console.log(doc);
+// }, (e) => {
+//     console.log('will not work')
+// });
